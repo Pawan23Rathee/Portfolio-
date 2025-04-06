c7d3f73ae9b8372ee5e5df71696506e0092c1ddb
Pawan Rathee <ratheepawan666@gmail.com>
2025-04-06 12:31:52 +0530

    Changes

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -3,7 +3,7 @@ export const projects = [
   {
     title: "Vaccination Record and Reminder",
     description: "A web application to manage vaccination records and send reminders.",
-    image: "./public/Vaccination Record and Reminder.png", // Place images in public/images
+    image: "./Vaccination Record and Reminder.png", // Place images in public/images
     tech: ["React", "Tailwind CSS", "Framer Motion"],
     demo: "https://vaccination-record-reminder-website.vercel.app",
     code: "https://github.com/Pawan23Rathee/Vaccination-Record-Reminder-Website.git",
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: "movie-showtime-app",
     description: "A web application to find movie showtimes and book tickets.",
-    image: "./public/Movie Booking app.png", // Place images in public/images
+    image: "./Movie Booking app.png", // Place images in public/images
     tech: ["React", "Tailwind CSS", "JavaScript"],
     demo: "https://movie-showtime-app.vercel.app/movies",
     code: "https://github.com/Pawan23Rathee/movie-showtime-app.git",
